a0890e536bc50ad46aabb42aec15b2b8079aef96
Kudryavtsevaa <Kudryavtsevaa@408D-7>
2023-05-06 11:58:52 +0700

    Добавьте файлы проекта.

---
+++ Delegat/MathExpressions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegat
+{
+    class MathExpressions
+    {
+        private delegate double MathOperation(double value); //определение делегата MathOperation
+
+        private static double Add(double value) { return value + 10; }   //определение статических методов, которые будут использоваться в качестве элементарных операций
+        private static double Subtract(double value) { return value - 5; }
+        private static double Multiply(double value) { return value * 3; }
+        private static double Divide(double value) { return value / 2; }
+        private static double Power(double value) { return Math.Pow(value, 2); }
+
+        private static MathOperation[] operations = new MathOperation[]//массив из 10 делегатов на элементарные операции
+        {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Power,
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Power
+        };
+
+        public double CalculateMathExpression(double value) //метод для последовательного вызова операций из массива указателей на методы
+        {
+            foreach (var operation in operations)
+            {
+                value = operation(value);
+            }
+            return value;
+        }
+    }
+}
+
---
+++ Delegat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathExpressions expressions = new MathExpressions();
+
+            double result1 = expressions.CalculateMathExpression(2.5);
+            double result2 = expressions.CalculateMathExpression(-10);
+            double result3 = expressions.CalculateMathExpression(0);
+
+            Console.WriteLine($"Результат1: {result1}");
+            Console.WriteLine($"Результат2: {result2}");
+            Console.WriteLine($"Результат3: {result3}");
+
+            Console.ReadKey();
+        }
+    }
+}
